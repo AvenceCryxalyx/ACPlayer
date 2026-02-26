ca0caf22382a4a702b525745d527fe5562a56412
AvenceCryxalyx <avencecryxalyx@gmail.com>
2026-02-26 23:54:07 +0800

    stuttering issue fix. slider and duration display fix

--- ACPlayer/ACPlayer.cpp
+++ ACPlayer/ACPlayer.cpp
@@ -6,10 +6,20 @@ ACPlayer::ACPlayer(QWidget *parent)
     ui.setupUi(this);
 
     Player = new QMediaPlayer();
-
-    audio = new QAudioOutput();
-    Player->setAudioOutput(audio);
+    if (!audio)
+    {
+        audio = new QAudioOutput();
+    }
     
+    if (!Video)
+    {
+        Video = new QVideoWidget();
+        Video->setGeometry(5, 5, ui.groupBox_Video->width() - 10, ui.groupBox_Video->height() - 10);
+        Video->setParent(ui.groupBox_Video);
+    }
+    Player->setAudioOutput(audio);
+    Player->setVideoOutput(Video);
+
     ui.slider_volume->setMinimum(0);
     ui.slider_volume->setMaximum(100);
     ui.slider_volume->setValue(50);
@@ -32,9 +42,9 @@ ACPlayer::ACPlayer(QWidget *parent)
     connect(ui.pushButton_Mute, &QPushButton::clicked, this, &ACPlayer::on_muteToggled);
     connect(ui.slider_volume, &QAbstractSlider::valueChanged, this, &ACPlayer::on_hSlider_Volume_valueChanged);
     connect(ui.slider_progress, &QAbstractSlider::valueChanged, this, &ACPlayer::on_hSlider_Progress_valueChanged);
-
     connect(Player, &QMediaPlayer::durationChanged, this, &ACPlayer::durationChanged);
     connect(Player, &QMediaPlayer::positionChanged, this, &ACPlayer::positionChanged);
+    connect(Player, &QMediaPlayer::positionChanged, this, &ACPlayer::updateProgressPosition);
     ui.slider_progress->setRange(0, Player->duration() / 1000);
 }
 
@@ -72,18 +82,30 @@ void ACPlayer::on_muteToggled()
 
 void ACPlayer::on_playToggled()
 {
+    if (Player->mediaStatus() == 0)
+    {
+        return;
+    }
+    else if(Player->mediaStatus() == QMediaPlayer::MediaStatus::EndOfMedia)
+    {
+        Player->setPosition(0);
+        Player->play();
+    }
+
     if (Is_Paused)
     {
         ui.pushButton_Play->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
-        Is_Paused = false;
+        ui.pushButton_Play->setText("Pause");
         Player->play();
     }
-    else
+    else if(!Is_Paused)
     {
         ui.pushButton_Play->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
-        Is_Paused = true;
+        ui.pushButton_Play->setText("Play");
         Player->pause();
     }
+
+    Is_Paused = !Player->isPlaying();
 }
 
 void ACPlayer::on_nextPressed()
@@ -107,6 +129,11 @@ void ACPlayer::on_hSlider_Volume_valueChanged(int value)
     audio->setVolume(newValue);
 }
 
+void ACPlayer::on_QPlayer_durationChanged(qint64 position)
+{
+
+}
+
 void ACPlayer::on_hSlider_Progress_valueChanged(int value)
 {
     Player->setPosition(value * 1000);
@@ -120,25 +147,14 @@ void ACPlayer::OpenWithFile(QString* fileName)
 
 void ACPlayer::PlayVideo(QString* fileName)
 {
-    if (!Video)
-    {
-        Video = new QVideoWidget();
-        Video->setGeometry(5, 5, ui.groupBox_Video->width() - 10, ui.groupBox_Video->height() - 10);
-        Video->setParent(ui.groupBox_Video);
-        Player->setVideoOutput(Video);
-    }
-    else
+    if(Player->isPlaying())
     {
         Player->stop();
     }
     Player->setSource(QUrl(*fileName));
     Video->setVisible(true);
 
-    //while (Player->mediaStatus() != QMediaPlayer::MediaStatus::LoadedMedia)
-    //{//
-    //}
-
-    Player->play();
+    on_playToggled();
 }
 
 void ACPlayer::on_actionOpenTriggered()
@@ -152,27 +168,37 @@ void ACPlayer::durationChanged(qint64 duration)
 {
     mDuration = duration / 1000;
     ui.slider_progress->setMaximum(mDuration);
+
+    QString Format = "hh:mm:ss";
+    if (mDuration <= 3600) Format = "mm:ss";
+
+    QTime TotalTime((mDuration / 3600) % 60, (mDuration / 60) % 60, (mDuration * 1000) % 1000);
+    ui.label_time_left->setText(TotalTime.toString(Format));
 }
 void ACPlayer::positionChanged(qint64 duration)
 {
     if (!ui.slider_progress->isSliderDown())
     {
-        ui.slider_progress->setValue(duration / 1000);
+        ui.slider_progress->blockSignals(true);
+        ui.slider_progress->setValue(duration/1000);
+        ui.slider_progress->blockSignals(false);
     }
-    updateDuration(duration / 1000);
 }
 
-void ACPlayer::updateDuration(qint64 duration)
+void ACPlayer::updateProgressPosition(qint64 duration)
 {
+
     if (duration || mDuration)
     {
-        QTime CurrentTime((duration / 3600) % 60, (duration / 60) % 60, (duration * 1000) % 1000);
-        QTime TotalTime((mDuration / 3600) % 60, (mDuration / 60) % 60, (mDuration * 1000) % 1000);
+        int formatDividend1 = millisecondsPerSecond;
+        int formatDividend2 = millisecondsPerSecond * secondsPerMinute;
+        int formatDividend3 = millisecondsPerSecond * secondsPerMinute * minutesPerHour;
+
+        QTime CurrentTime((duration/ formatDividend3), (duration / formatDividend2) % 60,(duration / formatDividend1) % 60);
+        
         QString Format = "hh:mm:ss";
         if (mDuration <= 3600) Format = "mm:ss";
-
         ui.label_time_elapsed->setText(CurrentTime.toString(Format));
-        ui.label_time_left->setText(TotalTime.toString(Format));
     }
 }
 
--- ACPlayer/ACPlayer.h
+++ ACPlayer/ACPlayer.h
@@ -28,6 +28,7 @@ private slots:
     void on_muteToggled();
     void on_hSlider_Volume_valueChanged(int value);
     void on_hSlider_Progress_valueChanged(int value);
+    void on_QPlayer_durationChanged(qint64 position);
     //void initialize();
     void HandleStatusChange(QMediaPlayer::MediaStatus Status) {
         emit statusChanged(Status); // Redirect so that the main application can handle this signal too
@@ -47,6 +48,10 @@ private:
     bool Is_Paused = true;
     bool Is_Muted = false;
 
-    void updateDuration(qint64 duration);
+    void updateProgressPosition(qint64 duration);
+
+    const int millisecondsPerSecond = 1000;
+    const int secondsPerMinute = 60;
+    const int minutesPerHour = 60;
 };
 
